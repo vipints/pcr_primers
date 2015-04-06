aa5320db0bad8a9a87b73c33069fff51dafd4735
Vipin <vipin@cbio.mskcc.org>
2015-04-05 22:37:03 -0400

    starting the program

---
+++ design_primers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+"""
+wrapper for primer3 primer design program
+"""
+
+
+import os 
+import sys 
+from gfftools import GFFParser
+
+def main():
+    """
+    core function
+    """
+    
+    gtf_fname = ""
+    gene_name = ""
+
+    gtf_content = GFFParser.Parse(gtf_fname)
+    
+    ## extract the gene of interest 
+
+
+if __name__=="__main__":
+    main()
