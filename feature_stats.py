b829f21678d259b8b3fb1b699666460860e11825
Vipin <vipin@cbio.mskcc.org>
2015-06-22 15:19:34 -0400

    fixed and tested hs mm genome to get the feature details

--- feature_stats.py
+++ feature_stats.py
@@ -35,12 +35,12 @@ def get_features(gtf_file, fa_file):
             else:
                 non_coding += 1
 
-        for idx, trans in enumerate(feat['transcripts']):
-            if feat['utr3_exons'][idx].any() and feat['utr5_exons'][idx].any():
+        for idx, trans in enumerate(features['transcripts']):
+            if features['utr3_exons'][idx].any() and features['utr5_exons'][idx].any():
                 utr +=1
-            elif feat['utr3_exons'][idx].any():
+            elif features['utr3_exons'][idx].any():
                 utr3 +=1
-            elif feat['utr5_exons'][idx].any():
+            elif features['utr5_exons'][idx].any():
                 utr5 += 1
             else:
                 no_utr +=1 
@@ -49,30 +49,18 @@ def get_features(gtf_file, fa_file):
     print 'total transcripts: ', total_transcripts
     print 'coding transcripts: ', cds_coding
     print 'noncoding transcripts: ', non_coding
-    print ''
+    print '---------------'
+    print 'both utrs', utr 
+    print '3 utr ', utr3
+    print '5 utr ', utr5
+    print 'no utr ', no_utr
 
-    """
-    cds_idx = []
-    for idp, feat in enumerate(anno_db):
-        if not feat['cds_exons'][0].any():
-            cds_idx.append(idp)
-    
-    # total coding genes 
-    # total noncoding genes 
-
-    cds_exons = 0 
-    utr3_exons = 0 
-
-    for feat in anno_db:
-        if not feat['cds_exons'][0].any():
-            cds_exons += 1 
-        if not feat['utr3_exons'][0].any():
-            utr3_exons += 1 
-    """
 
 if __name__=="__main__":
 
-    gtf_fname = "data/H_sapiens/GRCh38/out.gff"
+    #gtf_fname = "data/H_sapiens/GRCh38/out.gff"
+    gtf_fname = "data/M_musculus/GRCm38/out.gff"
+
     fas_fname = "data/H_sapiens/GRCh38/out.fas.bz2"
 
     get_features(gtf_fname, fas_fname)
